31db031d4ecf7cee46e46fda6b257fdad561278f
jullia-beatriz <jullia_beatriz@outlook.com>
2021-03-24 19:05:17 -0300

    falacia o maior hello word teste commit

---
+++ teste03git.py
@@ -0,0 +1,4 @@
+print(''' 
+O maior print Hello Word! ja criado para testar um commit!
+autor: eu mesma!                                    
+                                    (e verdade esse bilhete!)''')
