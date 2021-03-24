0c8e12ee208fc1de27794b56e95078531acf90e6
jullia-beatriz <jullia_beatriz@outlook.com>
2021-03-24 19:45:57 -0300

    alteracao convidos inputados por usuario funcionando!

--- teste02git.py
+++ teste02git.py
@@ -1,6 +1,10 @@
-convidados = ('Julia ''20 anos','Daniel ''27 anos')
+convidados = list()
 tot = 0
 
-for p in convidados :
-    tot +=1
-    print(f'Seja bem vindo(a)! {p}')
+for pessoas in range(0,5):
+    convidados.append(input('Digite nome e Sobrenome: '))
+    tot += 1
+
+
+print(f'Seja bem vindo(a)! {convidados}'.title())
+print(f'No total foram cadastrados {tot} convidados!')
